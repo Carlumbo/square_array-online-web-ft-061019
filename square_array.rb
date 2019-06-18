7d8bd99b40a4a42cc465dbe32f649df0ca2a88b0
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 05:31:36 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,5 @@
-def sqaure_array(balls)
+def sqaure_array(old_balls)
   new_balls = []
-  balls.each {|oldballs| new_balls <<   }
+  old_balls.each {|balls| new_balls << balls ** 2   }
+  new_balls 
+end
