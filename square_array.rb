874477f368e804f7abdbf87e83fc4fc0685fbf46
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 05:39:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,13 @@
-def square_array(old_balls)
-  new_balls = []
-  old_balls.each {|balls| new_balls << balls ** 2   }
-  new_balls 
+def square_array(array)
+  new_array = []
+  array.each {|element| new_array << balls ** 2   }
+  new_array
+  puts "#{new_array}"
 end
 
 numbers = [1,2,3]
 square_array(numbers)
+
+new_numbers = [9,10,16,25]
+
+sqaure_array(new_numbers)
