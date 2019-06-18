0fb1e30f53a105e9798f6add5f1143011e829c13
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-18 05:26:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 
 
 def square_array(array)
-    array = []
+    new_array = []
     array.each do |number| 
-      new_array<< number**2 
+      new_array << number**2 
+      new_array
     end
 end
 
